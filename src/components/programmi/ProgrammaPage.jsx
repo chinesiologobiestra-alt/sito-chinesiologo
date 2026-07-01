c3ea868383ad71183662d0aeb0f51b249e2e7cc8
chinesiologobiestra-alt <chinesiologo.biestra@gmail.com>
2026-07-01 12:52:38 +0200

    programma

--- src/components/programmi/ProgrammaPage.jsx
+++ src/components/programmi/ProgrammaPage.jsx
@@ -2,21 +2,17 @@ export default function ProgrammaPage({ children }) {
   return (
     <div className="flex justify-center my-4 bg-zinc-100">
 
-     <div
-  className="
-    pdf-page
-    bg-white
-    w-[297mm]
-    min-h-[210mm]
-    p-[8mm]
-    box-border
-    border
-    border-zinc-300
-  "
->
-        style={{
-          boxSizing: "border-box",
-        }}
+      <div
+        className="
+          pdf-page
+          bg-white
+          w-[297mm]
+          min-h-[210mm]
+          p-[8mm]
+          box-border
+          border
+          border-zinc-300
+        "
       >
         {children}
       </div>
